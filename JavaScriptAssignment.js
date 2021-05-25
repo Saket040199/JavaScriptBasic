49582c117acbbf0c825183b202bc28df7f758997
saket <saketj420@gmail.com>
2021-05-25 14:01:16 +0530

    Adding Sequence UC4

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -12,3 +12,12 @@ console.log(dice1)
 let dice2=Math.floor(Math.random()*10) %6 +1
 console.log(dice2)
 console.log(dice1+dice2);
+
+//UC4
+let noSize=5;
+let sum=0;
+for(let no=0;no<5;no++){
+sum += Math.floor(Math.random()*100)
+} 
+let avg=sum/noSize;
+console.log("Sum "+sum +"  avg " +avg);
