16b430266c883e658cb9259a04567832c0ced646
saket <saketj420@gmail.com>
2021-05-25 13:49:18 +0530

    Adding sequence UC3

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -1,7 +1,14 @@
 //Sequence UC1
-let digit = Math.floor(Math.random()*10)
-console.log(digit);
+// let digit = Math.floor(Math.random()*10)
+// console.log(digit);
 
-//UC2
-let dice=Math.floor(Math.random()*10) %6 +1
-console.log(dice);
+// UC2
+// let dice=Math.floor(Math.random()*10) %6 +1
+// console.log(dice);
+
+//UC3
+let dice1=Math.floor(Math.random()*10) %6 +1
+console.log(dice1)
+let dice2=Math.floor(Math.random()*10) %6 +1
+console.log(dice2)
+console.log(dice1+dice2);
