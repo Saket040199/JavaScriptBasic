5169cdde57fd8ce39bae833d01a502396dbb8ed5
saket <saketj420@gmail.com>
2021-05-26 11:20:40 +0530

    Selection UC8

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -161,3 +161,32 @@ console.log("Area of 25 plots in Acres : "
     }
     numberUnitCheck(1)
     numberUnitCheck(5000)
+
+    //Selection UC8
+    let a=3;
+    let b =15;
+    let c =6;
+    let value1 = a+b*c;
+    let value2 = a%b+c;
+    let value3 = c+a/b;
+    let value4 = a*b+c;
+    
+    if(value1 > value2 && value1 > value3 && value1 > value4){
+        console.log("Max: "+"a+b*c = "+value1);
+    }else if(value2> value1 && value2 > value3 && value2 > value4){
+        console.log("Max: "+"a%b+c = "+value2);
+    }else if(value3 > value1 && value3 > value2 && value3 > value4){
+        console.log("Max: "+"c+a/b= "+value3);
+    }else {
+        console.log("Max: "+"a*b+c= "+value4);
+    }
+
+    if(value1 < value2 && value1 < value3 && value1 < value4){
+        console.log("Min: "+"a+b*c= "+value1);
+    }else if(value2 < value1 && value2 < value3 && value2 < value4){
+        console.log("Min: "+"a%b+c= "+value2);
+    }else if(value3 < value1 && value3 < value2 && value3 < value4){
+        console.log("Min: "+"c+a/b= "+value3);
+    }else {
+        console.log("Min: "+"a*b+c= "+value4);
+    }
