8256876c1f810bf6782eba9f21704819744abec6
saket <saketj420@gmail.com>
2021-05-26 12:12:50 +0530

    WhileLoop UC4

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -392,3 +392,19 @@ console.log("Area of 25 plots in Acres : "
     console.log(headCount>tailCount?"Heads Win":"Tails Win");
     console.log("No of heads= "+headCount);
     console.log("No of tails= "+tailCount);
+
+    //While UC4
+    let money=100;
+    let betsPlaced=0;
+    let winCount=0;
+    do{
+       betsPlaced++;
+       let bet=Math.floor(Math.random()*10) % 2;
+       if (bet==1){
+           money++; winCount++;
+       }else{
+           money--;
+       }
+    }while(money>0 && money<200);
+    console.log("You have Rs "+money+" in the end");
+    console.log("You won "+winCount+" out of "+betsPlaced+" bets");
