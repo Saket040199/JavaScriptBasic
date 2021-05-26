6189fceda12b1c59464f48553b53b26a73953b6f
saket <saketj420@gmail.com>
2021-05-26 11:52:51 +0530

    ForLoop UC4

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -307,3 +307,20 @@ console.log("Area of 25 plots in Acres : "
     checkPrime(9);
     checkPrime(11);
     checkPrime(57);
+
+    //ForLoop UC4
+    function getPrime(num){
+        for (let i = 3; i < num; i++) {
+            let primeFlag=true;
+            for (var j = 2; j < i; j++){
+                if (i % j == 0) {
+                    primeFlag=false;
+                    break;
+                }
+            }
+            if(primeFlag) console.log(i+" is a Prime Number");
+        }
+    }
+    getPrime(9);
+    getPrime(18);
+    getPrime(21);
