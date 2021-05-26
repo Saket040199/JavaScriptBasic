306971f80c10329e21bdbfc028fd7e7c52801d4c
saket <saketj420@gmail.com>
2021-05-26 11:29:09 +0530

    Switch UC1

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -190,3 +190,31 @@ console.log("Area of 25 plots in Acres : "
     }else {
         console.log("Min: "+"a*b+c= "+value4);
     }
+
+    //Selection Switch UC1
+    function numCheck(num){
+        switch(num){
+            case 1: console.log("ONE");
+             break;
+            case 2: console.log("TWO");
+             break;
+            case 3: console.log("THREE");
+             break;
+            case 4: console.log("FOUR");
+             break;
+            case 5: console.log("FIVE");
+             break;
+            case 6: console.log("SIX");
+             break;
+            case 7: console.log("SEVEN");
+             break;
+            case 8: console.log("EIGHT");
+             break;
+            case 9: console.log("NINE"); 
+            break;
+            default: console.log("Invalid Number");
+        }
+    }
+    numCheck(7);
+    numCheck(2);
+    numCheck(420);
