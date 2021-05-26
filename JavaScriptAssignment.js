70e13dc021635228e3ae91e91bade8122c7c4218
saket <saketj420@gmail.com>
2021-05-26 12:07:03 +0530

    WhileLoop UC2

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -362,3 +362,22 @@ console.log("Area of 25 plots in Acres : "
     getPowerSeries(4);
     getPowerSeries(7);
     getPowerSeries(14);
+
+    //While UC2
+    let max=100;
+    let min=1;
+    let num=Math.floor(Math.random()*100)+1;
+    console.log("Generated number: "+num);
+    let mid
+    do{
+       mid=Math.floor(((min+max)/2));
+       if(num<mid){
+           max=mid;
+           console.log(num+" is less than "+mid);
+       }else if(num>mid){
+          min=mid;
+          console.log(num+" is more than "+mid);
+       }
+    }while(num!=mid);
+    console.log("Found "+mid);
+    
