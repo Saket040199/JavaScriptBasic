8548716ccd1a9018f371050b9f4a961a86cf51c4
saket <saketj420@gmail.com>
2021-05-26 11:36:54 +0530

    Switch UC4

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -251,3 +251,17 @@ console.log("Area of 25 plots in Acres : "
     unitCheck(365);
     unitCheck(10000);
     
+    //Switch UC4
+    function unitConversion(input,value){
+        switch(input){
+            case 1: console.log("Feet To Inch: "+value+" ft = "+(value*12)+" in"); break;
+            case 2: console.log("Feet To Metre: "+value+" ft = "+(value*0.3048)+" m"); break;
+            case 3: console.log("Inch To Feet: "+value+" in = "+(value/12)+" ft"); break;
+            case 4: console.log("Metre To Feet: "+value+" m = "+(value*3.28084)+" ft"); break;
+            default: console.log("Invalid Number");
+        }
+    }
+    unitConversion(1,24);
+    unitConversion(2,24);
+    unitConversion(3,24);
+    unitConversion(4,24);
