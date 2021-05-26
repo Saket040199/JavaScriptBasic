53ac76563f790e53daffe9599a29c2e268300b01
saket <saketj420@gmail.com>
2021-05-26 12:17:05 +0530

    Function UC2

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -429,3 +429,21 @@ console.log("Area of 25 plots in Acres : "
     }
     getTemp("degreeC",Math.floor(Math.random()*100));
     getTemp("degreeF",Math.floor((Math.random()*181)+32));
+
+    //Function UC2
+    function checkPalindrome(num1,num2){
+        let revDigits=0;
+        let num=num1;
+        while(num>0){
+            let digit = num%10;
+            revDigits = revDigits*10 +digit;
+             num=Math.floor(num/10);
+        }
+        if(revDigits == num2) 
+            console.log(num1+" and "+num2+" are Palindromes");
+        else
+            console.log(num1+" and "+num2+" are NOT Palindromes");
+    }
+    checkPalindrome(256,652);
+    checkPalindrome(141,414);
+    checkPalindrome(404,404);
