41554aa5881d30c77edff9c1485739b5ce7481ac
saket <saketj420@gmail.com>
2021-05-26 11:49:44 +0530

    ForLoop UC3

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -290,3 +290,20 @@ console.log("Area of 25 plots in Acres : "
     getHarmonicSum(8);
     getHarmonicSum(12);
     
+    //ForLoop UC3
+    function checkPrime(num){
+        let primeFlag=true;
+        for (let i = 2; i < num/2; i++) {
+            if (num % i == 0) {
+                console.log(num+" is NOT a Prime Number");
+                primeFlag=false;
+                break;
+            }
+        }
+        if (num>1 && primeFlag){
+            console.log(num+" is a Prime Number");
+        }
+    }
+    checkPrime(9);
+    checkPrime(11);
+    checkPrime(57);
