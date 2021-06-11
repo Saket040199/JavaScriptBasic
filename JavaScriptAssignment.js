bde3c9106395ecc99db54c5c2fc9f611b369f5ea
saket <saketj420@gmail.com>
2021-06-11 12:00:29 +0530

    Adding Map UC2

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -616,3 +616,25 @@ for (var i=1;i<=6;i++){
     }
 }
 console.log("Got min value:"+minCount+", for number "+min1);
+
+//UC2
+let birthMonthMap=new Map();
+let monthBirthMap=new Map();
+for (var i=1;i<=12;i++){
+    monthBirthMap.set(i,new Array());
+}
+for (var i=1;i<=50;i++){
+    let month=Math.floor((Math.random()*12)+1);
+    birthMonthMap.set("Person "+i,month);
+    for (var j=1;j<=12;j++){
+        if (month==j){
+            let newArr=monthBirthMap.get(j);
+            newArr.push("Person "+i);
+            monthBirthMap.set(j,newArr);
+        }
+    }
+}
+console.log("Map containing input data:");
+console.log(birthMonthMap);
+console.log("Map with people having same birth month:");
+console.log(monthBirthMap);
