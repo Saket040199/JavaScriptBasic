4454ac364bf14521d4a510c0839fb5643ec78158
saket <saketj420@gmail.com>
2021-06-11 11:48:57 +0530

    Adding Array UC3

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -532,3 +532,21 @@ function getPrimeFactors(num) {
 getPrimeFactors(5);
 getPrimeFactors(10);
 getPrimeFactors(14);
+
+//Array UC3
+let intArray=[1,2,-5,4,3,-3];
+let triplet=new Array();
+let output1=new Array();
+for(var x=0;x<intArray.length;x++){
+    for(var y=x;y<intArray.length;y++){
+        for(var z=y;z<intArray.length;z++){
+            if(intArray[x]+intArray[y]+intArray[z]==0){
+                triplet.push(intArray[x],intArray[y],intArray[z]);
+                output1.push(triplet);
+                triplet = [];
+            }
+        }
+    }
+}
+console.log("The triplets are: ");
+console.log(output1);
