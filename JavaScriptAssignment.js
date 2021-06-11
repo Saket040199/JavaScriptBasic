60fb15859e438c9ef93f669f32c3a2738326fe2b
saket <saketj420@gmail.com>
2021-06-11 11:36:05 +0530

    Adding Array UC1

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -484,3 +484,34 @@ console.log("Area of 25 plots in Acres : "
     main(17);
     main(22);
     main(23);
+
+ //Array In JavaScript 
+
+//UC1
+let numArray=new Array();
+for (var i=1;i<=10;i++){
+    let random=(Math.floor(Math.random() * 900)+100);
+    numArray.push(random);
+}
+console.log("Selected Array= ");
+console.log(numArray);
+let firstLargest=numArray[0];
+let secondLargest=numArray[0];
+let firstSmallest=numArray[0];
+let secondSmallest=numArray[0];
+for (var i = 0; i < numArray.length; i++) {
+    if (numArray[i] > firstLargest) {
+        secondLargest = firstLargest;
+        firstLargest = numArray[i];
+    }else if (numArray[i] > firstLargest && numArray[i] != firstLargest) {
+        secondLargest = numArray[i];
+    }
+    if (numArray[i] < firstSmallest) {
+        secondSmallest = firstSmallest;
+        firstSmallest = numArray[i];
+    } else if (numArray[i] < secondSmallest && numArray[i] != firstSmallest) {
+        secondSmallest = numArray[i];
+    }
+}
+console.log("Second Largest Element: "+secondLargest);
+console.log("Second Smallest Element: "+secondSmallest);
