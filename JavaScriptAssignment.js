a7d431603c74f59ccb94eebf44c923dc0f63f340
saket <saketj420@gmail.com>
2021-06-11 11:38:09 +0530

    Adding Array UC2

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -515,3 +515,20 @@ for (var i = 0; i < numArray.length; i++) {
 }
 console.log("Second Largest Element: "+secondLargest);
 console.log("Second Smallest Element: "+secondSmallest);
+
+//Array UC2
+function getPrimeFactors(num) {
+    let number=num;
+    let factors=new Array();
+    for (var i = 2; i <= num; i++) {
+        while (num % i == 0) {
+            factors.push(i);
+            num = num / i;
+        }
+    }
+    console.log("The Prime Factors of "+number+" are:");
+    console.log(factors);
+}
+getPrimeFactors(5);
+getPrimeFactors(10);
+getPrimeFactors(14);
