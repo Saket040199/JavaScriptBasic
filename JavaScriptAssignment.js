27d3ffb30c3bac6a1a333e7a3efc5bd322be0d0b
saket <saketj420@gmail.com>
2021-06-11 11:53:45 +0530

    Adding Array UC4

--- JavaScriptAssignment.js
+++ JavaScriptAssignment.js
@@ -550,3 +550,23 @@ for(var x=0;x<intArray.length;x++){
 }
 console.log("The triplets are: ");
 console.log(output1);
+
+//Array UC4
+function checkPalindrome(num){
+    let revDigits=0;
+    let number=num;
+    while(num>0){
+        let digit = num%10;
+        revDigits = revDigits*10 +digit;
+        num=Math.floor(num/10);
+    }
+    if(revDigits == number) return true;
+    else return false;
+}
+
+let newArray=new Array();
+for(var i = 10; i <= 100; i++){
+    if(checkPalindrome(i)) newArray.push(i);
+}
+console.log("The Repeated digits between 0-100 are: ");
+console.log(newArray);
